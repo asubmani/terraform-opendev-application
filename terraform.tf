ac8db1f3ab7eded3a324d4e38dd13f200c4b2c1e
Nic Jackson <jackson.nic@gmail.com>
2017-10-21 14:20:57 +0100

    Updated remote state

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,8 @@ data "terraform_remote_state" "core" {
   backend = "azure"
   config {
     storage_account_name = "nictfremotestate"
-    container_name       = "quake-core"
-    key                  = "quake.terraform.tfstate"
+    container_name       = "opendev"
+    key                  = "core.terraform.tfstate"
   }
 }
 
